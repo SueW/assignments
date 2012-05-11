677dc2c4ebc1907acbbef599fd174fdeb237a75d
SueW <susanwoodford17@gmail.com>
2012-05-11 13:51:01 -0400

    Cleaned up the code in the general.js file

--- mtm1526/assignment-1/js/general.js
+++ mtm1526/assignment-1/js/general.js
@@ -7,19 +7,10 @@ while (!letter) {
 
 console.log(letter);
 
-/*
-if (name == ''|| name == null)
-if (name != '' && name != null)
-if (name)
-if (!name)
-*/
-
 
 if (letter){
 	for (var i = 0; i < 10; i++) {
-//writes some text or HTML into the <body>
-//need a space after Hello (inside quote) to provide a space between 
-//hello and name
+
 document.write(letter + '<p>');
 
 	}
