3f032245a50b535cfc0085acead26a17ef4d717b
SueW <susanwoodford17@gmail.com>
2012-05-14 15:30:38 -0400

    added index.php file

---
+++ mtm1531/assignment-2/index.php
@@ -0,0 +1,21 @@
+<?php
+
+var_dump($_POST);
+
+?><!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Money Calculator Form</title>
+	</head>
+
+<body>
+
+	<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') : ?>
+	<form method="post" action="index.php">
+	
+
+
+
+</body>
+</html>
