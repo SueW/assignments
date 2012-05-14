f73831439bd53e20446527022164a523b0b1115a
SueW <susanwoodford17@gmail.com>
2012-05-14 15:55:46 -0400

    Added number fields.

--- mtm1531/assignment-2/index.php
+++ mtm1531/assignment-2/index.php
@@ -14,6 +14,12 @@ var_dump($_POST);
 	<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') : ?>
 	<form method="post" action="index.php">
 	
+		<label for="number">Number 1</label>
+		<input type="number" name="number" min="1"/>
+		
+		<label for="number">Number 2</label>
+		<input type="number" name="number" min="1"/>
+		
 
 
 
