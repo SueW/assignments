64ad5b904f0fb895dab65768639a5043e7afe109
wood0022 <susanwoodford17@gmail.com>
2012-05-30 15:06:14 -0400

    added fieldset for Preferred Languages

--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -29,7 +29,28 @@ require_once 'includes/form-processor.php';
 			<input id="username" name="usernamename" required value="<?php echo $username; ?>">
 		</div>
 		
+		<div>
+			<label for="password">Password <?php if (isset($errors['password'])) : ?><strong class="error">is required</strong><?php endif; ?></label>
+			<input id="password" name="password" required value="<?php echo $password; ?>">
+		</div>
 		
+		<fieldset>
+			<legend>Preferred Language:</legend>
+			<?php if(isset($errors['preferredlang'])) : ?><strong>Please select your preferred language</strong><?php endif; ?>
+			
+			<input type="radio" id="english" name="preferredlang" value="english" <?php if($preferredlang == 'english') {echo 'checked';}?>>
+			<label for ="english">English</label>
+			
+			<input type="radio" id="french" name="preferredlang" value="french" <?php if($preferredlang == 'french') {echo 'checked';}?>>
+			<label for ="french">French</label>
+			
+			<input type="radio" id="spanish" name="preferredlang" value="spanish" <?php if($preferredlang == 'spanish') {echo 'checked';}?>>
+			<label for ="french">Spanish</label>
+		</fieldset>
+		
+		
+					
+	</form>
 	
 <?php endif; ?>
 	
