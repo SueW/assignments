f9a1c980accc489e41a7aa70cbbeb98208d1c135
wood0022 <susanwoodford17@gmail.com>
2012-05-30 15:04:42 -0400

    added name, email & username fields.

--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -12,6 +12,26 @@ require_once 'includes/form-processor.php';
 
 <body>
 
+	<form method="post" action="index.php">
+	
+		<div>
+			<label for="name">Name <?php if (isset($errors['name'])) : ?><strong class="error">is required</strong><?php endif; ?></label>
+			<input id="name" name="name" required value="<?php echo $name; ?>">
+		</div>
+			
+		<div>
+			<label for="email">E-mail Address<?php if (isset($errors['email'])) : ?><strong class="error"> is required</strong><?php endif; ?></label>
+			<input type="email" id="email" name="email" required value="<?php echo $email; ?>">
+		</div>
+		
+		<div>
+			<label for="username">Username <?php if (isset($errors['username'])) : ?><strong class="error">a user name is required and must be less than 25 characters</strong><?php endif; ?></label>
+			<input id="username" name="usernamename" required value="<?php echo $username; ?>">
+		</div>
+		
+		
+	
+<?php endif; ?>
 	
 
 </body>
