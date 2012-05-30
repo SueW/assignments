53e3fd73a7f616caa4711cd3e0449b9252e558a9
wood0022 <susanwoodford17@gmail.com>
2012-05-30 15:17:07 -0400

    added 'if' statements.

--- mtm1531/assignment-3/includes/form-processor.php
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -10,3 +10,39 @@ $preferredlang = filter_input(INPUT_POST, 'preferredlang', FILTER_SANITIZE_NUMBE
 $notes = filter_input(INPUT_POST, 'notes', FILTER_SANITIZE_STRING); 
 
 
+if ($_SERVER['REQUEST_METHOD'] =='POST') {
+	
+	// 0 is considered empty
+	// Empty: 0, false, null, '', arraay(), '0'
+	
+	if (empty($name)) {
+		$errors['name'] = true;
+	
+	
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+		$errors['email'] = true;
+	
+	
+	if (strlen($username) < 1 || strlen($username) > 25) 
+		$errors['username'] = true;
+	
+	
+	if (empty($password)) 
+		$errors['password'] = true;
+	
+	
+	if (!in_array($preferredlang, array('english','french','spanish')))
+		$errors['preferredlang'] = true;
+	
+	
+	
+	if (!isset($_POST['acceptterms']))
+		$errors['acceptterms'] = true;
+		
+			
+	if (empty($errors)) 
+		$headers = 'From: ' . $name . ' <' . $email . '>';
+		mail('susanwoodford17@gmail.com', $prefferredlang, $notes, $headers);
+	
+	}
+}
