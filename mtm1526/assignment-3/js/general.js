e43b8a557d56820c4676476cd256d6357d413a87
wood0022 <susanwoodford17@gmail.com>
2012-05-30 22:22:11 -0400

    added code

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,11 +1,19 @@
-$(document).ready(function () {
+jQuery(function ($) {
+	var $circle = $('.circle')
+		, $property = $('#property')
+		, $color = $('#color');
+
+	$('form').on('submit', function (e) {
+		var color = $color.val();
+
+		e.preventDefault();
+
+		if (color) {
+			$general.css($property.val(), color);
+		}
+	});
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	$('#hide-show').on('click', function (e) {
+		$ball.toggle(500);
+	});	
 });
