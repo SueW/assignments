2f64e9e30db12a066b3715e777cbb9ba7ab03aca
SueW <susanwoodford17@gmail.com>
2012-06-04 15:45:38 -0400

    added content to single.php

---
+++ mtm1531/assignment-5/single.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+// ->prepare() allows us to have variables in our SQLK
+// We can create placeholders and later fill them with some content
+// By using prepare we are protecting against SQL injection attacks
+// Placeholders are marked with a colon (:) in front of them
+
+$sql = $db->prepare('
+	SELECT id, name, release_date, director, motion_picture_rating
+	FROM movies
+	WHERE id =	:id
+');
+
+//bindValue(placeholder, variable, datatype)
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
+$sql->execute();
+$results = $sql->fetch();
+
+?><!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $results['name']; ?> &middot;Movies</title>
+	</head>
+	
+	<body>
+	
+	
+	<h1><?php echo $results['name']; ?></h1>
+	<dl>
+		<dt>Release Date</dt>
+			<dd><?php echo $results['release_date']; ?></dd>
+			<dt>Director</dt>
+			<dd><?php echo $results['director']; ?></dd>
+			<dt>Motion Picture Rating</dt>
+			<dd><?php echo $results['motion_picture_rating']; ?></dd>
+	</dl>
+	
+	<a href="delete.php?id=<?php echo $id; ?>">Delete</a>
+	
+	
+	</body>
+	
+	
+	</html>
