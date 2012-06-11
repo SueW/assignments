a710ea0cceab0b4a3e2d82188f4c7b57f0a35732
SueW <susanwoodford17@gmail.com>
2012-06-11 15:23:49 -0400

    Created assignment-6 folder and copied over all the files from assignment-5 folder

---
+++ mtm1531/assignment-6/index.php
@@ -0,0 +1,50 @@
+<?php
+
+
+require_once 'includes/db.php';
+
+$sql = $db->query('SELECT id, name, release_date, director, motion_picture_rating FROM movies ORDER BY name ASC');
+//the above just points to the results but still to fetch them (see below)
+
+$results = $sql->fetchAll();
+
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Top Flicks from the 80s</title>
+		<link href="css/general.css" rel="stylesheet" >
+	</head>
+	
+	<body>
+		<h1>Top Flicks from the 80s</h1>
+			<p>...the best era EVER</p>
+		
+		<?php foreach ($results as $flicks) : ?> 
+		<h2>
+			<a href="single.php?id=<?php echo $flicks['id']; ?>">
+				 <?php echo $flicks['name'] ?></a></h2>
+		<dl>
+			<dt>Release Date:</dt>
+			<dd><?php echo $flicks['release_date']; ?></dd>
+			<dt>Director:</dt>
+			<dd><?php echo $flicks['director']; ?></dd>
+			<dt>Motion Picture Rating:</dt>
+			<dd><?php echo $flicks['motion_picture_rating']; ?></dd>
+		</dl>
+		<?php endforeach; ?>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</body>
+</html>
